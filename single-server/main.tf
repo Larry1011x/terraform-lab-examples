f2e76356c0928c500b2f3a1e7f5dbc8eaa610b1e
Larry1011x <larry1011x@gmail.com>
2024-11-17 22:02:31 -0500

    created a security group and attached to the ec2 instance.

--- single-server/main.tf
+++ single-server/main.tf
@@ -2,9 +2,28 @@ provider "aws" {
     region = "us-east-2"
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress = [
+        {
+            description = "HTTP"
+            from_port = 8080
+            to_port = 8080
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = false
+        }
+    ]
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0942ecd5d85baa812"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.instance.id ]
 
     user_data = <<-EOF
               #!/bin/bash
